49c45d9f308e9123662e6c8e177295ee99d68358
Ryu <nishidaryu416@gmail.com>
2016-08-03 09:41:47 +0000

    tmp

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160731054923) do
+ActiveRecord::Schema.define(version: 20160803093359) do
 
   create_table "microposts", force: :cascade do |t|
     t.integer  "user_id"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 20160731054923) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "location"
+    t.string   "profile"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
