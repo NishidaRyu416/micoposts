d709b709b40d0c81b49fdd26a1e6b755b34adaeb
Ryu <nishidaryu416@gmail.com>
2016-08-03 08:44:51 +0000

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   get 'sessions/new'
 
   root to: 'static_pages#home'
-  get 'signup',  to: 'users#new'
+  get    'signup', to: 'users#new'
   get    'login' , to: 'sessions#new'
   post   'login' , to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   resources :users do
     member do
+      
     get 'followings', 'followers'
     end
   end
